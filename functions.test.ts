af1da715a0fc4e17b84e3ac004273d0894d66ef3
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-03-24 17:04:38 -0700

    added two test in functions.test.js

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,27 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
-})
+    it('results of shuffle array return same number of elements as its inputs', () => {
+        const input = [5, 10, 15, 20, 25];
+        const output = shuffleArray(input);
+        expect(output.length).toEqual(input.length);
+    });
+});
+
+describe('shoppinglist', () => {
+    it('Check to see if eggs are still on the shopping list', () => {
+        const listInput = [
+            'bread',
+            'eggs',
+            'chocolate milk',
+            'sliced cheese',
+            'sliced meat',
+            'orange juice'
+        ]
+        const shoppingList = shuffleArray(listInput);
+            expect(shoppingList).toContain('eggs');
+            expect(new Set(shoppingList)).toContain('eggs');
+    });
+    
+});
+
